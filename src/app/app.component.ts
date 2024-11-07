4daf8857d5a5caaa9b2db1e264c61fca6e31cf58
Diego Bonura <diego@bonura.dev>
2024-11-07 21:34:23 +0100

    Add few tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,59 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { PrimeNGConfig } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { DatePickerModule } from 'primeng/datepicker';
+import { InputTextModule } from 'primeng/inputtext';
+import { definePreset } from 'primeng/themes';
+
+import { Aura } from 'primeng/themes/aura';
+
+const DiegoPreset = definePreset(Aura, {
+  semantic: {
+    colorScheme: {
+      dark: {
+        primary: {
+          color: 'red'
+        }
+      }
+    }
+  },
+  components: {
+    button: {
+      colorScheme: {
+        dark: {
+          root: {
+            secondary: {
+              color: 'var(--caep-button-secondary-color)',
+              background: 'var(--caep-button-secondary-background)'
+            }
+          }
+        }
+      }
+    }
+  }
+});
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [DatePickerModule, ButtonModule, FormsModule, InputTextModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'primeng_v18';
+  date = new Date();
+
+  constructor(private config: PrimeNGConfig) {
+    this.config.theme.set({
+      preset: DiegoPreset,
+      options: {
+        //prefix: 'primeng',
+        cssLayer: {
+          name: 'primeng',
+          order: 'tailwind-base, primeng, tailwind-utilities'
+        }
+      }
+    })
+  }
 }
